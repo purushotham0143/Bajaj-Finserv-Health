87f59df37a20a4f7b304fbc83ffc92e0aa5deb1a
purushotham0143 <rpurushotham0143@gmail.com>
2025-07-29 21:17:28 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
